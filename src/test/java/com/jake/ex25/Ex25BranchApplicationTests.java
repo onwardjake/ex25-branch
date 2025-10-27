f2f0b1bb0afdea18b44f7c08d32c9c1c9a1b6b5b
onwardjake <onwardjake@gmail.com>
2025-10-27 11:29:07 +0900

    Initial commit

---
+++ src/test/java/com/jake/ex25/Ex25BranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.ex25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex25BranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
