2fe5aa290709558eb835c297022141854e04d505
onwardjake <onwardjake@gmail.com>
2025-10-28 11:35:33 +0900

    server port 변경, DBconn 코드 추가
    서버 포트 변경 : 8082
    DBConn 코드 추가 : DB 연결 코드

---
+++ src/main/java/com/jake/ex25/controller/DBConnTest.java
@@ -0,0 +1,25 @@
+package com.jake.ex25.controller;
+
+import java.sql.Connection;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DBConnTest {
+	@Autowired
+	private DataSource dataSource;
+	
+	@GetMapping("/dbconn")
+	public String dbconn() {
+		try(Connection conn = dataSource.getConnection()) {
+			String result = conn.getMetaData().getURL();
+			return "DB 연결 성공 : " + result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "DB 연결 실패.";
+		}
+	}
+}
