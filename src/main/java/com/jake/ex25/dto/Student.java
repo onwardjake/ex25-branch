c76965ac52141132a192642cc13d6950fe16477b
onwardjake <onwardjake@gmail.com>
2025-10-28 17:10:36 +0900

    URL 수정

--- src/main/java/com/jake/ex25/dto/Student.java
+++ src/main/java/com/jake/ex25/dto/Student.java
@@ -6,12 +6,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-//@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
