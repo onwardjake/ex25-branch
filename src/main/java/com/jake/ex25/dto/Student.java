08e1fc3388760df1dbb7261c9c43278c851ebe18
onwardjake <onwardjake@gmail.com>
2025-10-28 12:36:49 +0900

    validation 코드 추가

--- src/main/java/com/jake/ex25/dto/Student.java
+++ src/main/java/com/jake/ex25/dto/Student.java
@@ -2,20 +2,30 @@ package com.jake.ex25.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+//@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
     private Long id;
+    
+    @NotBlank(message = "이름은 필수 항목입니다.")
     private String name;
+    
+    @Email(message = "이메일 양식에 맞게 입력해주세요.")
     private String email;
+    
+    @Min(value = 1, message = "나이는 1 이상으로 입력해주세요.")
     private Integer age;
+    
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
