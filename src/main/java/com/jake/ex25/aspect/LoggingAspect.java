a3cf020b65b9997d7fac015de2b3d1f030496289
onwardjake <onwardjake@gmail.com>
2025-10-30 17:11:49 +0900

    Squashed commit of the following:
    commit c190738b6e516a4a624df926b88466639e182d36
    Author: onwardjake <onwardjake@gmail.com>
    Date:   Thu Oct 30 11:28:29 2025 +0900
    
        aop 코드 추가
    
        logging
        performance
        exception

---
+++ src/main/java/com/jake/ex25/aspect/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.jake.ex25.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+	
+	@Before("execution (* com.jake.ex25.service.*.*(..))")
+	public void logBefore(JoinPoint jp) {
+		log.info("========== 메소드 실행 전 : {} ==========", jp.getSignature());
+	}
+	
+	@AfterReturning(pointcut = "execution (* com.jake.ex25.service.*.*(..))", returning="result")
+	public void logAfter(JoinPoint jp, Object result) {
+		log.info("========== 메소드 실행  : {} /// 결과 : {} ==========", jp.getSignature(), result);
+	}
+}
